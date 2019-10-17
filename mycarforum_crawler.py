8afee0ba7f0edc03a197dad22711e9b0ee970b70
leeyuankang <leeyuankang94@gmail.com>
2019-10-17 21:21:56 +0800

    added additional crawled json files and added feature in mycarforum_crawler.py to loop through different forum thread based on url.

--- mycarforum_crawler.py
+++ mycarforum_crawler.py
@@ -37,47 +37,54 @@ def make_thread_object(thread_soup):
 # Get start time with current date and time 
 start_time = datetime.datetime.utcnow()
 
-# Create comment list to store the crawled data 
-comment_list = []
-
-# set counter to track crawling  progress
-count = 1
-
 # url for the thread to crawl 
-thread_url = "https://www.mycarforum.com/forums/topic/2703413-mercedes-owners-thread/"
-topic = thread_url[thread_url.find("topic/") + 6:-1]
+thread_url_list = ["https://www.mycarforum.com/forums/topic/2715945-mercedes-amg-most-powerful-4-cylinders-engine/",
+              "https://www.mycarforum.com/forums/topic/2668830-new-2nd-generation-mercedes-b-class/",
+              "https://www.mycarforum.com/forums/topic/2699114-mercedes-glc/",
+              "https://www.mycarforum.com/forums/topic/2717175-2019-mercedes-benz-glb-x247/",
+              "https://www.mycarforum.com/forums/topic/2709111-2020-mercedes-benz-maybach-gls-x167/"]
 
-# Request from url and convert to BeautifulSoup Object
-thread_soup = make_soup(thread_url)
-userid_url, comment_url, thread_page = make_thread_object(thread_soup)
-next_page_no, next_page_url = find_next_page(thread_page)
+for thread_url in thread_url_list:
+    # set counter to track crawling  progress
+    count = 1
 
-# Find the last page url to stop the loop
-last_page = thread_page.find(name='li', class_='ipsPagination_last')
-last_page = last_page.select('a')
-last_page_url = last_page[0].get('href')
-last_page_no = int(last_page_url[last_page_url.find("page=") + 5:])
+    # Create comment list to store the crawled data 
+    comment_list = []
 
-while (next_page_no <= last_page_no):
+    topic = thread_url[thread_url.find("topic/") + 6:-1]
+    print("Start crawling from", topic)
 
-    sleep_time = random.randint(1,7)
-    print(f'Sleeping time {sleep_time}s')
-    time.sleep(sleep_time)
-    print('Crawling page:', count, 'of', last_page_no)
-
-    # Storing into dictionary 
-    for i in range(len(userid_url)):
-        comment_dict = {}
-        comment_dict['userid'] = userid_url[i].get_text()
-        comment_dict['comment'] = comment_url[i].get_text()
-        comment_list.append(comment_dict)
-
-    next_page_no, next_page_url = find_next_page(thread_page)
-    thread_soup = make_soup(next_page_url)
+    # Request from url and convert to BeautifulSoup Object
+    thread_soup = make_soup(thread_url)
     userid_url, comment_url, thread_page = make_thread_object(thread_soup)
-    count += 1
+    next_page_no, next_page_url = find_next_page(thread_page)
 
-with open('./data/%s.json' % topic, 'w') as file:
-    json.dump(comment_list, file, indent=4, sort_keys=False)
-end_time = datetime.datetime.utcnow()
-print("Total time taken (mins): " +str(((end_time-start_time).total_seconds() / 60)))
+    # Find the last page url to stop the loop
+    last_page = thread_page.find(name='li', class_='ipsPagination_last')
+    last_page = last_page.select('a')
+    last_page_url = last_page[0].get('href')
+    last_page_no = int(last_page_url[last_page_url.find("page=") + 5:])
+
+    while (next_page_no <= last_page_no):
+
+        sleep_time = random.randint(1,7)
+        print(f'Sleeping time {sleep_time}s')
+        time.sleep(sleep_time)
+        print('Crawling page:', count, 'of', last_page_no)
+
+        # Storing into dictionary 
+        for i in range(len(userid_url)):
+            comment_dict = {}
+            comment_dict['userid'] = userid_url[i].get_text()
+            comment_dict['comment'] = comment_url[i].get_text()
+            comment_list.append(comment_dict)
+
+        next_page_no, next_page_url = find_next_page(thread_page)
+        thread_soup = make_soup(next_page_url)
+        userid_url, comment_url, thread_page = make_thread_object(thread_soup)
+        count += 1
+
+    with open('./data/%s.json' % topic, 'w') as file:
+        json.dump(comment_list, file, indent=4, sort_keys=False)
+    end_time = datetime.datetime.utcnow()
+    print("Total time taken (mins): " +str(((end_time-start_time).total_seconds() / 60)))
